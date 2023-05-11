a333689bae119aecec8e8fce778c857ca317929d
SANJAY U <112234870+Sanjayusa@users.noreply.github.com>
2023-05-11 13:55:23 +0530

    Add files via upload

--- assignment.cpp
+++ assignment.cpp
@@ -1,101 +1,78 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define MAX_SIZE 10
+#define MAX 10
 
-void printAssignment(int assignment[MAX_SIZE], int size)
-{
-    printf("Optimal Assignments:\n");
-    for (int i = 0; i < size; i++)
-    {
-        printf("Person %d -> Task %d\n", i + 1, assignment[i] + 1);
-    }
-}
+int n;
+int cost[MAX][MAX];
+bool assigned[MAX];
+int assignment[MAX];
 
-bool isSafe(int person, int task, int assignment[MAX_SIZE], int size, int costMatrix[MAX_SIZE][MAX_SIZE], bool assigned[MAX_SIZE])
-{
-    // Check if the task is already assigned to someone else
-    if (assigned[task])
-    {
-        return false;
-    }
-
-    // Check if the cost of assigning the task to the person is less than the current assignment
-    if (costMatrix[person][task] >= assignment[person])
-    {
-        return false;
-    }
-
-    return true;
-}
-
-void branchAndBoundUtil(int person, int assignment[MAX_SIZE], int size, int costMatrix[MAX_SIZE][MAX_SIZE], int currentCost, int *minCost, bool assigned[MAX_SIZE])
-{
-    if (person == size)
-    {
-        if (currentCost < *minCost)
-        {
-            *minCost = currentCost;
-            return;
+int calculateCost(int agent, int task) {
+    int totalCost = 0;
+    int i;
+    for (i = 0; i < n; i++) {
+        if (assignment[i] != -1) {
+            totalCost += cost[i][assignment[i]];
         }
     }
+    return totalCost + cost[agent][task];
+}
 
-    for (int task = 0; task < size; task++)
-    {
-        if (isSafe(person, task, assignment, size, costMatrix, assigned))
-        {
-            // Assign the task to the person
-            assigned[task] = true;
-            assignment[person] = task;
-            int newCost = currentCost + costMatrix[person][task];
-
-            branchAndBoundUtil(person + 1, assignment, size, costMatrix, newCost, minCost, assigned);
-
-            // Undo the assignment
-            assigned[task] = false;
-            assignment[person] = -1;
+void branchAndBound(int agent, int totalCost, int assignedTasks) {
+    int i;
+    for (i = 0; i < n; i++) {
+        if (!assigned[i]) {
+            assigned[i] = true;
+            assignment[agent] = i;
+            int newTotalCost = calculateCost(agent, i);
+            if (assignedTasks == n - 1) {
+                if (newTotalCost < totalCost) {
+                    totalCost = newTotalCost;
+                }
+            } else {
+                branchAndBound(agent + 1, totalCost, assignedTasks + 1);
+            }
+            assigned[i] = false;
+            assignment[agent] = -1;
         }
     }
 }
 
-void branchAndBound(int costMatrix[MAX_SIZE][MAX_SIZE], int size)
-{
-    int assignment[MAX_SIZE];
-    bool assigned[MAX_SIZE] = {false};
-    int minCost = __INT_MAX__;
-
-    branchAndBoundUtil(0, assignment, size, costMatrix, 0, &minCost, assigned);
-
-    if (minCost == __INT_MAX__)
-    {
-        printf("No feasible solution found.\n");
-    }
-    else
-    {
-        printAssignment(assignment, size);
-        printf("Minimum Cost: %d\n", minCost);
-    }
-}
-
-int main()
-{
-    int costMatrix[MAX_SIZE][MAX_SIZE];
-    int size;
-
-    printf("Enter the size of the matrix (square matrix): ");
-    scanf("%d", &size);
+void input() {
+    printf("Enter the number of agents and tasks (n): ");
+    scanf("%d", &n);
 
     printf("Enter the cost matrix:\n");
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            scanf("%d", &costMatrix[i][j]);
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &cost[i][j]);
         }
     }
+}
 
-    branchAndBound(costMatrix, size);
+void initialize() {
+    int i;
+    for (i = 0; i < n; i++) {
+        assigned[i] = false;
+        assignment[i] = -1;
+    }
+}
 
+void printAssignment() {
+    int i;
+    printf("Optimal Assignment:\n");
+    for (i = 0; i < n; i++) {
+        printf("Agent %d -> Task %d\n", i + 1, assignment[i] + 1);
+    }
+}
+
+int main() {
+    input();
+    initialize();
+    branchAndBound(0, 0, 0);
+    printAssignment();
     return 0;
 }
 
